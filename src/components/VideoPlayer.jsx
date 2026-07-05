5af2aec946aa5dff36e822b0fe86b4e1d0a07a16
Mohit Sundal <mohitsundal2006@gmail.com>
2026-07-05 15:40:36 +0530

    Sandbox iframe to prevent redirects

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -18,6 +18,7 @@ export default function VideoPlayer({ src }) {
           frameBorder="0"
           referrerPolicy="origin"
           title="Video Player"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-presentation"
         />
       </div>
     </div>
